ac0f72d98a3af05570ef1cbd4af1e23f1030cf4f
Shane Celis <shane.celis@gmail.com>
2025-02-24 17:24:44 -0500

    feature: Half-grid aligned walking.

--- main.lua
+++ main.lua
@@ -1,30 +1,105 @@
 world.info("Lua: The main.lua script just got loaded")
-function _init()
-end
 
+room = 0
 x = 0
 y = 0
-function _update()
-    if btn(0) then
-        x = x - 1
-    end
-    if btn(1) then
-        x = x + 1
-    end
-    if btn(2) then
-        y = y - 1
-    end
-    if btn(3) then
-        y = y + 1
+speed = 2
+function _init()
+    world.info("what")
+    if m == nil then
+        m = map(0, 0, 0, 0, 0, 0, nil, room):retain()
     end
 end
+function _update()
+    top_left_x = x / 16
+    top_left_y = y / 16
+    bottom_right_x = (x + 15) / 16
+    bottom_right_y = (y + 15) / 16
+    d = 1 / 16
+    if btn(0) then
+        if not is_wall(top_left_x - d, top_left_y,
+                              top_left_x - d, bottom_right_y) then
+        x = x - speed
+        end
+        y = y + grid_align(y)
+    end
+    if btn(1) then
+        if not is_wall(bottom_right_x + d, bottom_right_y,
+                              bottom_right_x + d, top_left_y) then
+        x = x + speed
+        end
+        y = y + grid_align(y)
+    end
+    if btn(2) then
+        if not is_wall(top_left_x, top_left_y - d,
+                              bottom_right_x, top_left_y - d) then
+            y = y - speed
+        end
+        x = x + grid_align(x)
+    end
+    if btn(3) then
+        if not is_wall(bottom_right_x, bottom_right_y + d,
+                              top_left_x, bottom_right_y + d) then
+        y = y + speed
+        end
+        x = x + grid_align(x)
+    end
+    if is_door(x, y) then
+        m:despawn()
+        room = 1
+        m = map(0, 0, 0, 0, 0, 0, nil, room):retain()
+        world.info("we at a door")
+    end
+end
+
+function d_mget(cx,cy,map_index)
+    local r = mget(cx,cy,map_index)
+    world.info("mget length " ..#r)
+    return r
+end
+
+function is_wall(cx, cy, ...)
+    local arg = {...}
+    local r = fgets(d_mget(cx, cy, room), 0)
+    for i = 1, #arg, 2 do
+        r = r or fgets(mget(arg[i], arg[i + 1], room), 0)
+    end
+    return r
+end
+
+function fgets(list, index)
+    local r = false;
+    for sprite in all(list) do
+        if sprite ~= nil then
+            r = r or fget(sprite, index)
+        end
+    end
+    return r
+end
+
+function is_door(x, y)
+    -- Check the center of the sprite.
+    local sprite = mget((x + 8) / 16, (y + 8) / 16, room)[1]
+    if sprite == nil then
+        return false
+    end
+    return fget(sprite, 1)
+end
+
+function grid_align(a)
+    local dist = a % 8
+    if dist == 0 then
+        return 0
+    elseif dist > 4 then
+        return 1
+    else
+        return -1
+    end
+end
+
 
 function _draw()
     cls(3)
-    if not m then
-        m = map(0, 0, 0, 0, 0, 0, nil, 0):retain()
-        m:name("hi")
-    end
     -- pset(x + 10,x, 2)
     spr(84, x, y)
 end
--- tilemap_packed.tsx
+++ tilemap_packed.tsx
@@ -1,9 +1,280 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="tilemap_packed" tilewidth="16" tileheight="16" tilecount="132" columns="12">
  <image source="tilemap_packed.png" width="192" height="176"/>
- <tile id="122">
+ <tile id="0" type="tile">
   <properties>
-   <property name="of" type="int" value="15"/>
+   <property name="p8flags" type="int" value="1"/>
   </properties>
  </tile>
+ <tile id="1" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="3" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="4" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="5" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="6" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="7" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="8" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="9" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="10" type="tile"/>
+ <tile id="11" type="tile"/>
+ <tile id="12" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="13" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="14" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="15" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="16" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="17" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="18" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="19" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="20" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="21" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="22" type="tile"/>
+ <tile id="23" type="tile"/>
+ <tile id="24" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="25" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="26" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="27" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="28" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="29" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="30" type="tile"/>
+ <tile id="31" type="tile"/>
+ <tile id="32" type="tile"/>
+ <tile id="33" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="34" type="tile"/>
+ <tile id="35" type="tile"/>
+ <tile id="36" type="tile"/>
+ <tile id="37" type="tile"/>
+ <tile id="38" type="tile"/>
+ <tile id="39" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="40" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="41" type="tile"/>
+ <tile id="42" type="tile"/>
+ <tile id="43" type="tile"/>
+ <tile id="44" type="tile"/>
+ <tile id="45" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="46" type="tile"/>
+ <tile id="47" type="tile"/>
+ <tile id="48" type="tile"/>
+ <tile id="49" type="tile"/>
+ <tile id="50" type="tile"/>
+ <tile id="51" type="tile"/>
+ <tile id="52" type="tile"/>
+ <tile id="53" type="tile"/>
+ <tile id="54" type="tile"/>
+ <tile id="55" type="tile"/>
+ <tile id="56" type="tile"/>
+ <tile id="57" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="58" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="59" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="60" type="tile"/>
+ <tile id="61" type="tile"/>
+ <tile id="62" type="tile"/>
+ <tile id="63" type="tile"/>
+ <tile id="64" type="tile"/>
+ <tile id="65" type="tile"/>
+ <tile id="66" type="tile"/>
+ <tile id="67" type="tile"/>
+ <tile id="68" type="tile"/>
+ <tile id="69" type="tile"/>
+ <tile id="70" type="tile"/>
+ <tile id="71" type="tile"/>
+ <tile id="72" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="73" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="74" type="tile"/>
+ <tile id="75" type="tile"/>
+ <tile id="76" type="tile"/>
+ <tile id="77" type="tile"/>
+ <tile id="78" type="tile"/>
+ <tile id="79" type="tile"/>
+ <tile id="80" type="tile"/>
+ <tile id="81" type="tile"/>
+ <tile id="82" type="tile"/>
+ <tile id="83" type="tile"/>
+ <tile id="84" type="tile"/>
+ <tile id="85" type="tile"/>
+ <tile id="86" type="tile"/>
+ <tile id="87" type="tile"/>
+ <tile id="88" type="tile"/>
+ <tile id="89" type="tile">
+  <properties>
+   <property name="p8flags" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="90" type="tile"/>
+ <tile id="91" type="tile"/>
+ <tile id="92" type="tile"/>
+ <tile id="93" type="tile"/>
+ <tile id="94" type="tile"/>
+ <tile id="95" type="tile"/>
+ <tile id="96" type="tile"/>
+ <tile id="97" type="tile"/>
+ <tile id="98" type="tile"/>
+ <tile id="99" type="tile"/>
+ <tile id="100" type="tile"/>
+ <tile id="101" type="tile"/>
+ <tile id="102" type="tile"/>
+ <tile id="103" type="tile"/>
+ <tile id="104" type="tile"/>
+ <tile id="105" type="tile"/>
+ <tile id="106" type="tile"/>
+ <tile id="107" type="tile"/>
+ <tile id="108" type="tile"/>
+ <tile id="109" type="tile"/>
+ <tile id="110" type="tile"/>
+ <tile id="111" type="tile"/>
+ <tile id="112" type="tile"/>
+ <tile id="113" type="tile"/>
+ <tile id="114" type="tile"/>
+ <tile id="115" type="tile"/>
+ <tile id="116" type="tile"/>
+ <tile id="117" type="tile"/>
+ <tile id="118" type="tile"/>
+ <tile id="119" type="tile"/>
+ <tile id="120" type="tile"/>
+ <tile id="121" type="tile"/>
+ <tile id="122" type="tile"/>
+ <tile id="123" type="tile"/>
+ <tile id="124" type="tile"/>
+ <tile id="125" type="tile"/>
+ <tile id="126" type="tile"/>
+ <tile id="127" type="tile"/>
+ <tile id="128" type="tile"/>
+ <tile id="129" type="tile"/>
+ <tile id="130" type="tile"/>
+ <tile id="131" type="tile"/>
 </tileset>
