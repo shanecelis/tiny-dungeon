397a3973ce970891a134149dc5aa820ae26f7934
Shane Celis <shane.celis@gmail.com>
2025-03-30 05:55:19 -0400

    feature: Use the world!

--- main.lua
+++ main.lua
@@ -6,44 +6,23 @@ room = 0
 x = 64
 y = 64
 speed = 1
-dir_y = 0
-dir_x = 0
+dy = 0
+dx = 0
 reach = 1
-player_size = 15
-player_adjust = (16 - player_size) / 16
 modal = false
-
+player_shape = { 2, -14,
+                 14, -2 }
+player_reach = { -2, -18,
+                 18, 2 }
 -- function on_script_loaded()
 --     if _init then
 --         _init()
 --     end
 -- end
-
--- position to map cell
-function pos_to_cell(x, y)
-    return x / 16, y / 16
-end
-
-function goto_room(room_num)
-    return
-    -- if m ~= nil then
-    --     m:despawn()
-    -- end
-    -- room = room_num
-    -- m = map(0, 0, 0, 0, 0, 0, nil, room):retain(0.1)
-    -- local props = mgetp("player_start", room, 1)
-    -- if props and props.x and props.y then
-    --     x = flr(props.x)
-    --     y = flr(props.y) - 16
-    --     -- y = flr(props.y)
-    -- end
-    -- world.info("changed room to "..room)
-end
-
+opened_chests = {}
 function _init()
     world.info("init")
     m = map(0, 0, 0, 0, 0, 0, nil, room):retain(0.1)
-    -- goto_room(room)
 end
 
 function _update()
@@ -53,55 +32,67 @@ function _update()
         end
         return
     end
-    top_left_x, top_left_y = pos_to_cell(x, y)
-    top_left_x = top_left_x + player_adjust
-    top_left_y = top_left_y + player_adjust
-    bottom_right_x, bottom_right_y = pos_to_cell(x + 15, y + 15)
-    bottom_right_x = bottom_right_x - player_adjust
-    bottom_right_y = bottom_right_y - player_adjust
-    d = 1 / 16
+    local dx, dy = 0, 0
     if btn(0) then
-        if not is_wall(top_left_x - d, top_left_y,
-                top_left_x - d, bottom_right_y) then
-            x = x - speed
-        end
-        dir_x = -1
-        dir_y = 0
+        dx = -1
     end
     if btn(1) then
-        if not is_wall(bottom_right_x + d, bottom_right_y,
-                bottom_right_x + d, top_left_y) then
-            x = x + speed
-        end
-        dir_x = 1
-        dir_y = 0
+        dx = 1
     end
     if btn(2) then
-        if not is_wall(top_left_x, top_left_y - d,
-                bottom_right_x, top_left_y - d) then
-            y = y - speed
-        end
-        dir_y = -1
-        dir_x = 0
+        dy = -1
     end
     if btn(3) then
-        if not is_wall(bottom_right_x, bottom_right_y + d,
-                top_left_x, bottom_right_y + d) then
-            y = y + speed
-        end
-        dir_y = 1
-        dir_x = 0
+        dy = 1
     end
-    local ids = ray(x,y)
+    local wall_dist, wall_id = min_dist(raycast(x + dx, y + dy, dx, dy, 1, player_shape))
+    if wall_dist and wall_dist < speed then
+        -- world.info("will bump ".. wall_dist.." "..props(wall_id))
+        world.info("will bump "..wall_id.." at dist "..wall_dist)
+    else
+        x = x + dx * speed
+        y = y + dy * speed
+    end
+
+    local ids = raydown(x + 8, y + 8)
     if #ids > 0 and btnp(4) then
+    -- if #ids > 0 then
         world.info("ray "..dump(ids).." "..dump(props(ids[1])))
     end
 
-    local cx, cy = pos_to_cell(x + 8 , y + 8 )
+    -- check for doors
+    for id in all(raydown(x + 8, y + 8, 2)) do
+        world.info("on door "..id)
+        local p = props(id)
+        if p and p.goto_place then
+            local v = place(p.goto_place)
+            if v then
+                x = v[1]
+                y = v[2] - 16
+            end
+        else
+            world.info("p "..dump(p))
+        end
+        -- p.k
+    end
+
+    -- local cx, cy = pos_to_cell(x + 8 , y + 8 )
     -- interact with something?
-    -- if btnp(5) then
-    --     local cx = cx + 0.5 + reach * dir_x
-    --     local cy = cy + 0.5 + reach * dir_y
+    if btnp(5) then
+        for id in all(raydown(x, y, 4, player_reach)) do
+            local p = props(id)
+            if p and p.class == "chest" then
+            end
+        end
+
+        local p = {}
+                cx, cy = camera()
+                rectfill(cx + 5, cy + 100, cx + 123, cy + 123)
+                print("You got "..(p.content or "nothing"), cx + 10, cy + 104, 0)
+                modal = true
+    end
+    --     local cx = cx + 0.5 + reach * dx
+    --     local cy = cy + 0.5 + reach * dy
     --     local props = mgetp({cx, cy}, room, 1)
     --     if props then
     --         -- world.info("props " .. dump(props))
@@ -119,54 +110,38 @@ function _update()
     --     end
     -- end
     -- Only grid align if we're moving in a single direction.
-    if abs(dir_y) ~ abs(dir_x) == 1 then
-        if dir_y ~= 0 then
-            x = x + grid_align(x)
-            dir_x = 0
-        else
-            y = y + grid_align(y)
-            dir_y = 0
-        end
-    end
+    -- if xor(dy == 0, dx == 0) then
+    --     if dy ~= 0 then
+    --         x = x + grid_align(x)
+    --     else
+    --         y = y + grid_align(y)
+    --     end
+    -- end
 
     -- check for exit tile
-    local props = mgetp({cx, cy}, room, 1)
-    if props and props.goto_level then
-        goto_room(props.goto_level)
-        world.info("we at a door")
-    end
-    camera(x - 64,y - 64)
-end
-
-function d_mget(cx, cy, map_index)
-    local r = mget(cx, cy, map_index)
-    world.info("mget length " .. #r)
-    return r
-end
-
-function is_block(r)
-    return false
-    -- if r and r.p8flags then
-    --     return r.p8flags == 1
-    -- elseif r and r.block then
-    --     return r.block == 1
-    -- else
-    --     return false
+    -- local props = mgetp({cx, cy}, room, 1)
+    -- if props and props.goto_level then
+    --     goto_room(props.goto_level)
+    --     world.info("we at a door")
     -- end
+    local qx = x/128
+    local qy = y/128
+    camera(128 * flr(qx),128 * flr(qy))
 end
 
-function is_wall(cx, cy, ...)
-    -- return false
-    local args = { ... }
-    if is_block(mgetp({cx, cy}, room, 0)) or is_block(mgetp({cx, cy}, room, 1)) then
-        return true
+function min_dist(list)
+    if list == nil then
+        return nil
     end
-    for i = 1, #args, 2 do
-        if is_block(mgetp({args[i], args[i + 1]}, room, 0)) or is_block(mgetp({args[i], args[i + 1]}, room, 1)) then
-            return true
+    local x = math.huge
+    local id = nil
+    for i = 2, #list, 2 do
+        if x > list[i] then
+            x = list[i]
+            id = list[i - 1]
         end
     end
-    return false
+    return x, id
 end
 
 function grid_align(a)
@@ -196,16 +171,18 @@ end
 function _draw()
     if not modal then
         cls()
+    else
+        return
     end
     -- pset(x + 10,x, 2)
     -- spr(84, x, y)
     local s = 99 -- sprite index
-    if dir_y < 0 then
+    if dy < 0 then
         s = {1, -- sprite sheet
              1  -- sprite index
         }
     end
 
-    spr(s, x, y, 1, 1, dir_x < 0)
+    spr(s, x, y, 1, 1, dx < 0)
 
 end
